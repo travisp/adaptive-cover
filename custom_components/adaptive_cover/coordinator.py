96c503f33f2d4ec5506fa88fc9ecf301899f003b
Travis <travisp@gmail.com>
2025-06-02 09:38:42 -0400

    Ignore state change when state change is unavailable

--- custom_components/adaptive_cover/coordinator.py
+++ custom_components/adaptive_cover/coordinator.py
@@ -224,6 +224,9 @@ class AdaptiveDataUpdateCoordinator(DataUpdateCoordinator[AdaptiveCoverData]):
         ]:
             _LOGGER.debug("Ignoring intermediate state change for %s", entity_id)
             return
+        if event.new_state.state == "unavailable":
+            self.logger.debug("New state is unavailable, not processing")
+            return
         if self.wait_for_target.get(entity_id):
             position = event.new_state.attributes.get(
                 "current_position"
